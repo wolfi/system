f9a912fef34ff27aee07a5d88af48c2fad85338d
Steven Wolf <jlwolfi@gmail.com>
2023-11-03 18:07:20 +0100

    key test

--- go.sh
+++ go.sh
@@ -6,6 +6,7 @@ sudo xbps-install -Su python3 python3-pipx
 # Install Ansible
 export PATH="$HOME/.local/bin:$PATH"
 pipx install --include-deps ansible
+ansible-galaxy collection install community.general
 
 # Run playbook
 ansible-playbook --ask-vault-pass local.yml
