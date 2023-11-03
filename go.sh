6763196e999b38acc42b3274cb84d5bbebd67c04
Steven Wolf <jlwolfi@gmail.com>
2023-11-03 17:58:16 +0100

    ask for pass

--- go.sh
+++ go.sh
@@ -4,7 +4,8 @@
 sudo xbps-install -Su python3 python3-pipx
 
 # Install Ansible
-pipx ensurepath
+export PATH="$HOME/.local/bin:$PATH"
 pipx install --include-deps ansible
 
 # Run playbook
+ansible-playbook --ask-vault-pass local.yml
