b0d843994819464eff7d4577abfbbe9c595c09d5
Steven Wolf <steven.wolf+git@pm.me>
2024-02-01 11:09:33 +0100

    aur

--- go.sh
+++ go.sh
@@ -6,7 +6,7 @@ sudo pacman -Sy --noconfirm python python-pipx
 # Install Ansible
 export PATH="$HOME/.local/bin:$PATH"
 pipx install --include-deps ansible
-ansible-galaxy collection install community.general
+ansible-galaxy collection install community.general kewlfft.aur
 
 # Run playbook
 ansible-playbook --ask-vault-pass local.yml
