18916434bb46e4cfda100e59aa88d3d8c072e0cf
Steven Wolf <steven.wolf+git@pm.me>
2024-10-03 08:49:34 +0200

    env vars

--- wayland/env.sh
+++ wayland/env.sh
@@ -12,12 +12,18 @@ export QT_ENABLE_HIGHDPI_SCALING=1
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export QT_WAYLAND_RECONNECT=1
 export SDL_VIDEODRIVER=wayland
-export WLR_RENDERER=gles2
 export _JAVA_AWT_WM_NONREPARENTING=1
+export KWIN_DRM_NO_AMS=1
+export WLR_RENDERER=gles2
+
+# Nvidia stuff
+export LIBVA_DRIVER_NAME=nvidia
+export GBM_BACKEND=nvidia-drm
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
 
 # Prevent using hardware cursors in Wayland, forcing software-rendered cursors.
 # Uncomment if you experience issues with your cursor
-# export WLR_NO_HARDWARE_CURSORS 1
+export WLR_NO_HARDWARE_CURSORS 1
 
 # Allow the Wayland renderer to fall back to software rendering if necessary.
 # Uncomment if you are running in a virtual machine or have an underpowered GPU
