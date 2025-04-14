a2f7a2e3d1697f95731c666e6d481e16178a0491
Steven Wolf <steven.wolf+git@pm.me>
2025-04-14 13:52:20 +0200

    updates

--- jobs/yay.sh
+++ jobs/yay.sh
@@ -1,7 +1,10 @@
 echo "[Yay] Start"
 
-git clone https://aur.archlinux.org/yay.git /tmp/yay
-cd /tmp/yay
-makepkg -si
-
-echo "[Yay] Finished"
+if [ ! -d "/tmp/yay" ]; then
+  git clone https://aur.archlinux.org/yay.git /tmp/yay
+  cd /tmp/yay
+  makepkg -si
+  rm -rf /tmp/yay
+else
+  echo -e "\e[32m[Yay]\e[0m Already set up, skipping"
+fi
