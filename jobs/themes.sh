4b1f90b3d665d1c8887615d7cda0e8d7ac01265a
Steven Wolf <steven.wolf+git@pm.me>
2025-04-14 13:49:47 +0200

    updates

--- jobs/themes.sh
+++ jobs/themes.sh
@@ -1,9 +1,19 @@
 echo -e "\e[32m[Themes]\e[0m Installing GTK, icon and cursor themes..."
 
-THEMES=(
+PACMAN_THEMES=(
   adapta-gtk-theme
   arc-icon-theme
   papirus-icon-theme
 )
 
-install_pacman "${THEMES[@]}"
+install_pacman "${PACMAN_THEMES[@]}"
+
+AUR_THEMES=(
+  matcha-gtk-theme
+  qogir-gtk-theme
+  qogir-icon-theme
+  qogir-cursor-theme
+  tela-circle-icon-theme-all
+)
+
+install_aur "${AUR_THEMES[@]}"
