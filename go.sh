ffa52d88858f32c7e58098279d4a4cefefd59b4c
Steven Wolf <steven.wolf+git@pm.me>
2025-04-14 15:45:23 +0200

    clean up

--- go.sh
+++ go.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
 # Source utility functions required for the setup
-source ./utils/packages.sh
+source utils/packages.sh
 
-# Basic./s
-source ./jobs/ssh.sh
-source ./jobs/git.sh
-source ./jobs/yay.sh
-source ./jobs/base.sh
-source ./jobs/dotfiles.sh
-source ./jobs/terminal.sh
+# Basics
+source jobs/ssh.sh
+source jobs/git.sh
+source jobs/yay.sh
+source jobs/base.sh
+source jobs/dotfiles.sh
+source jobs/terminal.sh
+source jobs/mail.sh
 
 # Dev
-source ./jobs/containerization.sh
-source ./jobs/development.sh
+source jobs/containerization.sh
+source jobs/development.sh
 
-# Deskt./op environment
-source ./jobs/desktop.sh
-source ./jobs/themes.sh
-source ./jobs/fonts.sh
-source ./jobs/software.sh
-source ./jobs/gaming.sh
+# Desktop environment
+source jobs/desktop.sh
+source jobs/pkms.sh
+source jobs/themes.sh
+source jobs/fonts.sh
+source jobs/software.sh
+source jobs/gaming.sh
 
-# Clean./up job at the end
-source ./jobs/cleanup.sh
+# Cleanup job at the end
+source jobs/cleanup.sh
