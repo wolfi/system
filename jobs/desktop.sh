695057f6f0cad24af84aa16a892e2441fa6397a2
Steven Wolf <steven.wolf+git@pm.me>
2025-04-14 15:53:00 +0200

    more desktop

--- jobs/desktop.sh
+++ jobs/desktop.sh
@@ -6,19 +6,41 @@ DESKTOP=(
   bluez-utils
   ffmpeg
   foot
+  grim
+  imagemagick
+  light
+  lua
+  lua-posix
   lxappearance
   ly
+  mako
+  mpd
   mpv
+  pastel
   poppler
+  python-pywal
   river
   rofi
+  slurp
+  swaybg
+  swayidle
   swaylock
   thunar
   thunar-archive-plugin
   thunar-volman
+  viewnior
+  waybar
   wezterm
+  wf-recorder
+  wl-clipboard
+  wlr-randr
+  wlroots
   xdg-desktop-portal-gtk
   xdg-desktop-portal-wlr
+  xdg-playerctl
+  xfce-polkit
+  xorg-xwayland
+  yad
 )
 
 install_pacman "${DESKTOP[@]}"
@@ -30,4 +52,4 @@ xdg-user-dirs-update --force
 echo -e "\e[32m[Desktop]\e[0m Enable bluetooth service..."
 sudo systemctl enable bluetooth.service --now
 
-# TODO: lua lua-posix wlr-randr swaybg swayidle swaylock wlroots wl-clipboard waybar wofi foot mako grim slurp wf-recorder light yad thunar geany mpv mpd mpc viewnior imagemagick xfce-polkit xorg-xwayland xdg-desktop-portal-wlr playerctl pastel python-pywal rofi pulsemixer
+# TODO: lua lua-posix wlr-randr swaybg swayidle swaylock wlroots wl-clipboard waybar wofi foot mako grim slurp wf-recorder light yad thunar geany mpd mpc viewnior imagemagick xfce-polkit xorg-xwayland xdg-playerctl pastel python-pywal rofi pulsemixer
