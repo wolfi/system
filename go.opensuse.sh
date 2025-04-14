6412136a770dba73d38a3b533e69dd1550f86473
Steven Wolf <steven.wolf+git@pm.me>
2025-04-14 15:39:40 +0200

    mail

---
+++ go.opensuse.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Source utility functions required for the setup
+source utils/packages.sh
+
+# Basics
+source jobs/ssh.sh
+source jobs/git.sh
+source jobs/yay.sh
+source jobs/base.sh
+source jobs/dotfiles.sh
+source jobs/terminal.sh
+source jobs/mail.sh
+
+# Dev
+source jobs/containerization.sh
+source jobs/development.sh
+
+# Desktop environment
+source jobs/desktop.sh
+source jobs/themes.sh
+source jobs/fonts.sh
+source jobs/software.sh
+source jobs/gaming.sh
+
+# Cleanup job at the end
+source jobs/cleanup.sh
