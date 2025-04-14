fa15dee3abac869c90847a2a9be260ba10412005
Steven Wolf <steven.wolf+git@pm.me>
2025-04-14 14:58:47 +0200

    path

--- go.sh
+++ go.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 # Source utility functions required for the setup
-source utils/packages.sh
+source ./utils/packages.sh
 
-# Basics
-source jobs/ssh.sh
-source jobs/git.sh
-source jobs/yay.sh
-source jobs/base.sh
-source jobs/dotfiles.sh
-source jobs/terminal.sh
+# Basic./s
+source ./jobs/ssh.sh
+source ./jobs/git.sh
+source ./jobs/yay.sh
+source ./jobs/base.sh
+source ./jobs/dotfiles.sh
+source ./jobs/terminal.sh
 
 # Dev
-source jobs/containerization.sh
-source jobs/development.sh
+source ./jobs/containerization.sh
+source ./jobs/development.sh
 
-# Desktop environment
-source jobs/desktop.sh
-source jobs/themes.sh
-source jobs/fonts.sh
-source jobs/software.sh
-source jobs/gaming.sh
+# Deskt./op environment
+source ./jobs/desktop.sh
+source ./jobs/themes.sh
+source ./jobs/fonts.sh
+source ./jobs/software.sh
+source ./jobs/gaming.sh
 
-# Cleanup job at the end
-source jobs/cleanup.sh
+# Clean./up job at the end
+source ./jobs/cleanup.sh
