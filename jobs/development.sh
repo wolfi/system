23a3852d7f8b8a3b02a7f2783b8ce35e3bc8755f
Steven Wolf <steven.wolf+git@pm.me>
2025-04-14 14:35:50 +0200

    dev

--- jobs/development.sh
+++ jobs/development.sh
@@ -1,3 +1,23 @@
+echo -e "\e[32m[Development]\e[0m Setting up dirs..."
+mkdir -p $HOME/personal
+mkdir -p $HOME/work
+
+echo -e "\e[32m[Development]\e[0m Installing n..."
+if ! command -v n &>/dev/null; then
+  git clone https://github.com/tj/n.git /tmp/n
+  cd /tmp/n
+  PREFIX=$HOME/.local/n make install
+
+  # Install latest node LTS
+  N_PREFIX=$HOME/.local/n $HOME/.local/n/bin/n lts
+
+  # Enable pnpm
+  export PATH=PATH:$HOME/.local/n/bin corepack enable pnpm
+
+  # Clean up
+  rm -rf /tmp/n
+fi
+
 echo -e "\e[32m[Development]\e[0m Installing python..."
 PYTHON=(
   pyenv
@@ -11,3 +31,28 @@ GOLANG=(
   templ
 )
 install_pacman "${GOLANG[@]}"
+
+echo -e "\e[32m[Development]\e[0m Installing rust..."
+if ! command -v cargo &>/dev/null; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o /tmp/rustup.sh
+  chmod +x /tmp/rustup.sh
+  /tmp/rustup.sh -y
+fi
+
+echo -e "\e[32m[Development]\e[0m Installing neovim..."
+NEOVIM=(
+  luarocks
+  neovim
+  python-pip
+  python-pynvim
+  tree-sitter
+  tree-sitter-cli
+  wl-clipboard
+)
+install_pacman "${NEOVIM[@]}"
+
+# Install neovim npm package
+PATH="$HOME/.local/n/bin:$PATH" npm install -g neovim
+
+# Install neovim plugins
+nvim --headless "+Lazy! sync" +qa
