46e6c4778fa838a2244c66aa843b00d953e91012
Steven Wolf <steven@localhost.localdomain>
2025-04-22 16:36:21 +0200

    add opensuse package manager functions

--- utils/packages.sh
+++ utils/packages.sh
@@ -7,3 +7,13 @@ install_aur() {
   local packages=("$@")
   yes | yay --needed --noconfirm -S "${packages[@]}"
 }
+
+install_zypper() {
+  local packages=("$@")
+  sudo zypper in -y "${packages[@]}"
+}
+
+install_flatpak() {
+  local packages=("$@")
+  sudo flatpack install -y "${packages[@]}"
+}
