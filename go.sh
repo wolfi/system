5fdb73d2507724246ebdb6e8693cbcd3c657f6d4
Steven Wolf <684171+wolfi@users.noreply.github.com>
2026-02-25 11:39:11 +0100

    setup work branch

--- go.sh
+++ go.sh
@@ -8,31 +8,29 @@ source utils/packages.sh
 # Basics
 source jobs/ssh.sh
 source jobs/git.sh
-# source jobs/yay.sh
 source jobs/base.sh
 source jobs/dotfiles.sh
 source jobs/terminal.sh
 # source jobs/mail.sh
 
 # Dev
-source jobs/containerization.sh
-source jobs/development.sh
+# source jobs/containerization.sh
+# source jobs/development.sh
 
 # Desktop environment
-# source jobs/packman.sh
-source jobs/codecs.sh
+# source jobs/codecs.sh
 source jobs/desktop.base.sh
-# source jobs/desktop.river.sh
-source jobs/gpu.sh
-source jobs/pkms.sh
+source jobs/desktop.river.sh
+# source jobs/gpu.sh
+# source jobs/pkms.sh
 source jobs/themes.sh
 source jobs/fonts.sh
-source jobs/software.sh
-source jobs/gaming.sh
-source jobs/virtualization.sh
+# source jobs/software.sh
+# source jobs/gaming.sh
+# source jobs/virtualization.sh
 
 # Required at work
-source jobs/mstrash.sh
+# source jobs/mstrash.sh
 
 # Cleanup job at the end
 source jobs/cleanup.sh
