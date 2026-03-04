be0a7dd76add7702c75b5d32071ded4f93712f4e
Steven Wolf <684171+wolfi@users.noreply.github.com>
2026-03-04 09:30:02 +0100

    add dwl

---
+++ jobs/desktop.dwl.sh
@@ -0,0 +1,16 @@
+# Install desktop environment
+echo -e "\e[32m[Desktop]\e[0m Installing base packages..."
+
+DESKTOP=(
+  foot
+  # mpd # Music Player Daemon
+  mpv # Media player
+  # obs-studio
+  # obs-vkcapture
+  # obs-backgroundremoval
+  flatpak
+  flatpak-zsh-completion
+  xdg-user-dirs
+)
+
+install_zypper "${DESKTOP[@]}"
