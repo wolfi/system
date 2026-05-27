27f1e582c292ec48976762b0cce8292c6fd191de
Steven Wolf <684171+wolfi@users.noreply.github.com>
2026-05-27 15:10:21 +0200

    upd

--- go.sh
+++ go.sh
@@ -7,11 +7,11 @@ source utils/packages.sh
 source utils/config.sh
 
 # Basics
-# source jobs/ssh.sh
-# source jobs/git.sh
-# source jobs/base.sh
-# source jobs/dotfiles.sh
-# source jobs/terminal.sh
+source jobs/ssh.sh
+source jobs/git.sh
+source jobs/base.sh
+source jobs/dotfiles.sh
+source jobs/terminal.sh
 # source jobs/mail.sh
 
 # Dev
@@ -21,13 +21,13 @@ source utils/config.sh
 
 # Desktop environment
 # source jobs/packman.sh
-# source jobs/codecs.sh
+source jobs/codecs.sh
 source jobs/desktop.base.sh
-source jobs/desktop.river.sh
+# source jobs/desktop.river.sh
 # source jobs/gpu.sh
 # source jobs/pkms.sh
-# source jobs/themes.sh
-# source jobs/fonts.sh
+source jobs/themes.sh
+source jobs/fonts.sh
 # source jobs/software.sh
 # source jobs/gaming.sh
 # source jobs/virtualization.sh
