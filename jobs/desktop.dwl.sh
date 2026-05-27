3c95884b2a43146966227a465b195769a4bd0d30
Steven Wolf <684171+wolfi@users.noreply.github.com>
2026-05-27 13:59:16 +0200

    upd

--- jobs/desktop.dwl.sh
+++ jobs/desktop.dwl.sh
@@ -1,16 +1,53 @@
 # Install desktop environment
-echo -e "\e[32m[Desktop]\e[0m Installing base packages..."
+echo -e "\e[32m[Desktop]\e[0m Installing DWL..."
 
-DESKTOP=(
-  foot
-  # mpd # Music Player Daemon
-  mpv # Media player
-  # obs-studio
-  # obs-vkcapture
-  # obs-backgroundremoval
-  flatpak
-  flatpak-zsh-completion
-  xdg-user-dirs
+DWL_VERSION=v0.8
+WLROOTS_VERSION=0.19.2
+
+WLROOTS_DEPS=(
+  Mesa-libEGL-devel
+  Mesa-libGLESv2-devel
+  cairo-devel
+  glslang-devel
+  hwdata
+  libdisplay-info-devel
+  libdrm-devel
+  libdrm-tools
+  libgbm-devel
+  libinput-devel
+  liblcms2-devel
+  libliftoff-devel
+  libpixman-1-0-devel
+  libseat1
+  libxcb-devel
+  libxcb-errors0
+  libxcb-render-util0
+  libxkbcommon-devel
+  meson
+  meson-vim
+  seatd
+  seatd-devel
+  udev
+  wayland-devel
+  wayland-protocols-devel
+  xcb-util-errors-devel
+  xcb-util-renderutil-devel
+  xcb-util-wm-devel
+  xwayland-devel
 )
 
-install_zypper "${DESKTOP[@]}"
+install_zypper "${WLROOTS_DEPS[@]}"
+
+# Vulkan build depencies
+sudo zypper in -y -t pattern devel_vulkan
+
+# Build wlroots
+if ! command -v yay &>/dev/null; then
+  git clone -b $WLROOTS_VERSION https://gitlab.freedesktop.org/wlroots/wlroots.git /tmp/wlroots
+  cd /tmp/wlroots
+  meson build -Dprefix=/usr
+  sudo ninja -C build install
+  rm -rf /tmp/wlroots
+else
+  echo -e "\e[32m[Yay]\e[0m Already set up, skipping"
+fi
