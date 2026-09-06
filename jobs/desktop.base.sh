df5cdee759948e7b4202de02071bd9d5a4859f6f
Steven Wolf <684171+wolfi@users.noreply.github.com>
2026-09-06 14:44:54 +0200

    emacs

--- jobs/desktop.base.sh
+++ jobs/desktop.base.sh
@@ -11,6 +11,20 @@ DESKTOP=(
   flatpak
   flatpak-zsh-completion
   xdg-user-dirs
+  keyd
 )
 
 install_zypper "${DESKTOP[@]}"
+
+# Add keyd config to remap mouse buttons 1 and 2
+sudo echo "[ids]
+
+*
+04a5:8005:f618ad6b
+
+[main]
+
+mouse1 = leftshift
+mouse2 = leftcontrol" >/etc/keyd/default.conf
+
+sudo systemctl enable keyd
